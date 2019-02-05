d98e2bbf50ca484b9d5e99592650eb33c9a56fc4
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2019-02-06 01:20:17 +0900

    add_color

--- 0205sample.rb
+++ 0205sample.rb
@@ -1,18 +1,17 @@
 teams = ["ManU","ManC","Milan","real"]
 
-puts teams[1]
-
 teams.each {|team| puts "my favorite team is #{team}"}
 
-
-teams.each.with_index(2) do |team,q|
+teams.each.with_index(1) do |team,q|
   puts "No.#{q},#{team}"
 end
 
 puts teams.join (" and ")
 
-
 puts "what do you think which team is champion?? "
 champion = gets.chomp
 
 puts "I think that's #{champion}"
+
+colors = ["red","blue","redblack","white"]
+puts colors
